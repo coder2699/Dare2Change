79f223def00c1c0d17e239c1c5520ad7670243f8
ajaygalagali <gvajay321@gmail.com>
2020-12-03 20:59:32 +0530

    dashboard-frontend

---
+++ app/src/main/java/com/example/inout2020_aimers/ui/DashboardFragment.kt
@@ -0,0 +1,12 @@
+package com.example.inout2020_aimers.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.inout2020_aimers.R
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+
+}
