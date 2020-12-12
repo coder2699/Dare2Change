7494af5a6e0a3aaaede809d983fa08af66b86cd6
coder2699 <pranshu0414@gmail.com>
2020-12-12 19:20:32 +0530

    Added Articles

--- app/src/main/java/com/example/inout2020_aimers/Resources/ResourceFragment.kt
+++ app/src/main/java/com/example/inout2020_aimers/Resources/ResourceFragment.kt
@@ -17,6 +17,43 @@ class ResourceFragment : Fragment() {
 
     var ResourceList = mutableListOf(
         Resources(//Done
+            "50 Ways to Increase Productivity and Achieve More in Less Time"
+        ),
+        Resources(//Done
+            "The 80-20 rule - Pareto Principle"
+        ),
+        Resources(//Done
+            "Eisenhower's Urgent/Important Principle\n" +
+                    "Using Time Effectively, Not Just Efficiently"
+        ),
+        Resources(//Done
+            "10 smart productivity techniques you should know about"
+        ),
+        Resources(//Done
+            "Follow @imshivkhera\n" +
+                    "- Mr. Shiv Khera is an Author, Educator, Business Consultant and successful Entrepreneur, he is a much sought-after speaker."
+        ),
+        Resources(//Done
+            "Follow @RobinSharma\n" +
+                    "- One of world’s top leadership experts + Humanitarian + author of the #1 worldwide bestseller #The5amClub"
+        ),
+        Resources(//Done
+            "Follow @garyvee\n" +
+                    "- entrepreneur, investor, author, speaker"
+        ),
+        Resources(//Done
+            "Follow @GrantCardone\n" +
+                    "- Star of Undercover Billionaire, CEO CardoneCapital \$2.2B AUM, Author & Founder of 10X Movement."
+        ),
+        Resources(//Done
+            "Follow @BrianTracy\n" +
+                    "- Professional Speaker, Author, Success Expert, CEO of BrianTracy International & 14 Step Goal-Setting Guide"
+        ),
+        Resources(//Done
+            "Follow @OfficialJimRohn\n" +
+                    ", the man many consider to be America's Foremost Business Philosopher, shared his success philosophies and principles for over 46 years."
+        ),
+        Resources(//Done
             "Eat That Frog!: 21 Great Ways to Stop Procrastinating and Get More Done in Less Time"
         ),
         Resources(//Done
@@ -49,6 +86,7 @@ class ResourceFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_resource, container, false)
     }
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
